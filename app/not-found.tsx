49dd3a4837fb8be8fa0d9ab78a669ec69d1cdc45
mahiv <anubisnemesis643@gmail.com>
2024-05-15 17:45:30 +0600

    error page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1 style={{ fontWeight: 'bold', fontSize: '4rem' }}>404 Page Not Found</h1>
+    </div>
+  )
+}
